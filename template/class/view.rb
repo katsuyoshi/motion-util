65efe58e6239232f7a046fd3ff999b7cef251b0b
Katsuyoshi Ito <kito@itosoft.com>
2012-06-29 12:42:56 +0900

    added init/draw methods to view template

--- template/class/view.rb
+++ template/class/view.rb
@@ -1,3 +1,31 @@
 # -*- coding: utf-8 -*-
 class #{class_name}View < UIView
+
+=begin
+  def initWithFrame frame
+    super
+    self
+  end
+=end
+
+=begin
+  def initWithCorder decode
+    super
+    self
+  end
+=end
+
+=begin
+  def awakeFromNib
+  end
+=end
+
+=begin
+  def drawRect rect
+    context = UIGraphicsGetCurrentContext()
+    # Draw your code here.
+  end
+=end
+
 end
+
