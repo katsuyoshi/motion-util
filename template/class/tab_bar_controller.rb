b03e0a0782ed0fd39c53dd5287862de8b96379e5
Katsuyoshi Ito <kito@itosoft.com>
2012-07-13 18:52:52 +0900

    update template of view controllers.

--- template/class/tab_bar_controller.rb
+++ template/class/tab_bar_controller.rb
@@ -1,3 +1,35 @@
 # -*- coding: utf-8 -*-
 class #{class_name}TabBarController < UITabBarController
+
+=begin
+  def viewDidLoad
+    super
+  end
+=end
+
+=begin
+  def viewDidUnload
+    super
+  end
+=end
+
+=begin
+  def viewWillAppear animated
+    super
+  end
+=end
+
+=begin  
+  def viewWillDisappear animated
+    super
+  end
+=end
+  
+=begin
+  def shouldAutorotateToInterfaceOrientation interfaceOrientation
+    interfaceOrientation == UIInterfaceOrientationPortrait
+  end
+=end
+
+
 end
--- template/class/view_controller.rb
+++ template/class/view_controller.rb
@@ -1,3 +1,41 @@
 # -*- coding: utf-8 -*-
 class #{class_name}ViewController < UIViewController
+
+=begin
+  def initWithNibName nibNameOrNil, bundle:nibBundleOrNil
+    super
+    self
+  end
+=end
+  
+=begin
+  def viewDidLoad
+    super
+  end
+=end
+
+=begin
+  def viewDidUnload
+    super
+  end
+=end
+
+=begin
+  def viewWillAppear animated
+    super
+  end
+=end
+
+=begin  
+  def viewWillDisappear animated
+    super
+  end
+=end
+  
+=begin
+  def shouldAutorotateToInterfaceOrientation interfaceOrientation
+    interfaceOrientation == UIInterfaceOrientationPortrait
+  end
+=end
+
 end
